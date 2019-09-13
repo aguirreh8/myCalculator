912a38552d2da04ecd91869c5ee74adca4678747
unknown <aguirreh8@gmail.com>
2019-09-13 19:04:14 -0400

    Create Displya component, start adding styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './components/Header';
 import Main from "./components/Main";
+import Display from "./components/Display";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main>
-        <p>Main</p>
-      </Main>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                    <Main>
+                        <p>Main</p>
+                        <Display currentValue="123345"/>
+                    </Main>
+            </div>
+        )
+    };
 }
 
 export default App;
