5b936fda0cef37f1fad74db2e15b80e2605d8647
unknown <aguirreh8@gmail.com>
2019-09-14 19:21:49 -0400

    Add number buttons, add styling, change from button element to div

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,37 @@ import Main from "./components/Main";
 import Display from "./components/Display";
 
 class App extends Component {
+	state = {
+		currentValue: "0"
+	}
+
+	handleDisplayInput = (input) => {
+		this.setState({
+			currentValue: this.state.currentValue + input
+		})
+	}
+
   render() {
     return (
       <div className="App">
         <Header />
         <Main>
-          <Display currentValue="123345" />
+          <Display currentValue={this.state.currentValue} />
           <div className="keypad-layout">
-
+            <div className="keypad-numBtn-layout">
+				<div className="keypad-number-btn" >
+					1
+				</div>
+				<div className="keypad-number-btn">
+					2
+				</div>
+				<div className="keypad-number-btn">
+					3
+				</div>
+				<div className="keypad-number-btn">
+					4
+				</div>
+			</div>
           </div>
         </Main>
       </div>
