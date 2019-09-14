421ed21af0d6379f4da97ed9909baf2ae1bb06b8
unknown <aguirreh8@gmail.com>
2019-09-14 18:27:03 -0400

    Add KeypadLayout component, delete some extra code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import React, {Component} from 'react';
-import Header from './components/Header';
+import React, { Component } from "react";
+import Header from "./components/Header";
 import Main from "./components/Main";
 import Display from "./components/Display";
+import KeypadLayout from "./components/KeypadLayout";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Header />
-                    <Main>
-                        <p>Main</p>
-                        <Display currentValue="123345"/>
-                    </Main>
-            </div>
-        )
-    };
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main>
+          <Display currentValue="123345" />
+          <KeypadLayout>
+            <p>123</p>
+          </KeypadLayout>
+        </Main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/KeypadLayout/KeypadLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./KeypadLayout.css";
+
+const KeypadLayout = props => (
+    <div className="keypad-layout">
+        {props.children}
+    </div>
+)
+
+export default KeypadLayout;
